090b5b5829811e4e1e1d6ddc17c908ffca58795e
David Stinnett <davidstinnett@icloud.com>
2025-08-18 14:41:48 -0400

    added click handlers to modal forms so that they properly submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form")
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input")
 
 const newPostButton = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageLink = newPostForm.querySelector("#image-link-input");
+const newPostCaptionInput = newPostForm.querySelector("#image-caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 profileEditButton.addEventListener("click", function() {
+    editProfileNameInput.value = profileNameEl.textContent;
+    editProfileDescriptionInput.value = profileDescriptionEl.textContent;
     editProfileModal.classList.add("modal_is-opened");
 })
 
@@ -21,3 +32,21 @@ newPostButton.addEventListener("click", function() {
 newPostCloseButton.addEventListener("click", function() {
     newPostModal.classList.remove("modal_is-opened");
 })
+
+function handleEditProfileSubmit(evt) {
+    evt.preventDefault();
+    profileNameEl.textContent = editProfileNameInput.value;
+    profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+    editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit)
+
+function handleNewPostSubmit(evt) {
+    evt.preventDefault();
+    console.log(newPostImageLink.value);
+    console.log(newPostCaptionInput.value);
+    newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit)
