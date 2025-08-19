e35a20327175b272d9d35e4b5f2329808a53561c
David Stinnett <davidstinnett@icloud.com>
2025-08-19 18:56:59 -0400

    recieved feedback on my modal styling. Implemented tweaks to fix position of close button on mobile, change box-sizing on the form inputs, and remove to border from the submit button

--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,49 @@
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(".modal__close-btn");
-const editProfileForm = editProfileModal.querySelector(".modal__form")
-const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
-const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input")
+const editProfileForm = document.forms["edit-profile-form"];
+const editProfileNameInput = editProfileForm.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileForm.querySelector("#profile-description-input");
 
 const newPostButton = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
-const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostForm = document.forms["new-post-form"];
 const newPostImageLink = newPostForm.querySelector("#image-link-input");
 const newPostCaptionInput = newPostForm.querySelector("#image-caption-input");
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-function openProfileModal() {
-     editProfileModal.classList.add("modal_is-opened");
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
 }
 
-function closeProfileModal() {
-    editProfileModal.classList.remove("modal_is-opened");
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
 }
 
-function openPostModal() {
-    newPostModal.classList.add("modal_is-opened");
-}
+profileEditButton.addEventListener("click", () => {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  openModal(editProfileModal);
+});
 
-function closePostModal() {
-    newPostModal.classList.remove("modal_is-opened");
-}
+editProfileCloseButton.addEventListener("click", () => closeModal(editProfileModal));
+newPostButton.addEventListener("click", () => openModal(newPostModal));
+newPostCloseButton.addEventListener("click", () => closeModal(newPostModal));
 
-profileEditButton.addEventListener("click", function() {
-    editProfileNameInput.value = profileNameEl.textContent;
-    editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-    openProfileModal();
-})
+editProfileForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
+});
 
-editProfileCloseButton.addEventListener("click", function() {
-    closeProfileModal();
-})
-
-newPostButton.addEventListener("click", function() {
-    openPostModal();
-})
-
-newPostCloseButton.addEventListener("click", function() {
-    closePostModal();
-})
-
-function handleEditProfileSubmit(evt) {
-    evt.preventDefault();
-    profileNameEl.textContent = editProfileNameInput.value;
-    profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-    closeProfileModal();
-}
-
-editProfileForm.addEventListener("submit", handleEditProfileSubmit)
-
-function handleNewPostSubmit(evt) {
-    evt.preventDefault();
-    console.log(newPostImageLink.value);
-    console.log(newPostCaptionInput.value);
-    newPostForm.reset();
-    closePostModal();
-}
-
-newPostForm.addEventListener("submit", handleNewPostSubmit)
+newPostForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log(newPostImageLink.value);
+  console.log(newPostCaptionInput.value);
+  newPostForm.reset();
+  closeModal(newPostModal);
+});
