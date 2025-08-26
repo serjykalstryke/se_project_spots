a5b2505bf2b4ed44965f7da28cea8a1fe5a7e60c
David Stinnett <davidstinnett@icloud.com>
2025-08-26 19:54:56 -0400

    removed a console.log I forgot to get rid of before submitting. Also moved initialCards array to top with the variables.

--- scripts/index.js
+++ scripts/index.js
@@ -21,42 +21,7 @@ const previewImageEl = previewModal.querySelector(".modal__image");
 const previewCaptionEl = previewModal.querySelector(".modal__caption");
 
 const cardTemplate = document.querySelector("#card-template").content.querySelector(".card");
-const cardList = document.querySelector(".cards__list")
-function getCardElement(data) {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardTitleEl = cardElement.querySelector(".card__title");
-  const cardImageEl = cardElement.querySelector(".card__image");
-
-  cardImageEl.src = data.link;
-  cardImageEl.alt = data.name;
-  cardTitleEl.textContent = data.name;
-
-  const cardLikeBtnEl = cardElement.querySelector(".card__like-button");
-  cardLikeBtnEl.addEventListener("click", () => {
-    cardLikeBtnEl.classList.toggle("card__like-button_active")
-  })
-
-  const cardDeleteBtnEl = cardElement.querySelector(".card__delete-button");
-  cardDeleteBtnEl.addEventListener("click", () => {
-    cardElement.remove();
-  })
-
-cardImageEl.addEventListener("click", () => {
-  console.log("clicked");
-
-  previewImageEl.src = data.link;
-  previewImageEl.alt = data.name;
-  previewCaptionEl.textContent = data.name;
-
-  openModal(previewModal);
-})
-
-  return cardElement;
-}
-
-previewModalCloseBtn.addEventListener("click", () => {
-  closeModal(previewModal);
-});
+const cardList = document.querySelector(".cards__list");
 
 const initialCards = [
   {
@@ -88,6 +53,41 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"
   }
 ];
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardTitleEl.textContent = data.name;
+
+  const cardLikeBtnEl = cardElement.querySelector(".card__like-button");
+  cardLikeBtnEl.addEventListener("click", () => {
+    cardLikeBtnEl.classList.toggle("card__like-button_active")
+  })
+
+  const cardDeleteBtnEl = cardElement.querySelector(".card__delete-button");
+  cardDeleteBtnEl.addEventListener("click", () => {
+    cardElement.remove();
+  })
+
+cardImageEl.addEventListener("click", () => {
+  previewImageEl.src = data.link;
+  previewImageEl.alt = data.name;
+  previewCaptionEl.textContent = data.name;
+
+  openModal(previewModal);
+})
+
+  return cardElement;
+}
+
+previewModalCloseBtn.addEventListener("click", () => {
+  closeModal(previewModal);
+});
+
 
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
